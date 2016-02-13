7442cee8a0b837e3ba54ff3fd5a51df613965161
SlamDewey <mr.jaredmassa@gmail.com>
2016-02-13 15:03:28 -0500

    Revert "Revert "The project""
    This reverts commit e634a8fe6292d3ae717750a15611c43791f55d85.

---
+++ SoundGenerator/SoundGenerator/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = PATH.Text;
+            double hertz = Convert.ToDouble(HertzValue.Text);
+            WaveGenerator waveGenerator = new WaveGenerator(WaveExampleType.ExampleSineWave, hertz);
+            waveGenerator.save(filePath);
+            SoundPlayer soundPlayer = new SoundPlayer(filePath);
+            soundPlayer.Play();
+        }
+
+        private void PathLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HertzSlider_Scroll(object sender, EventArgs e)
+        {
+            HertzValue.Text = Convert.ToString(HertzSlider.Value);
+        }
+    }
+}
